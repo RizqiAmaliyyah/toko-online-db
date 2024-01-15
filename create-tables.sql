8ddbda752797c8da84e9360bc204bcfa2dee80f9
RizqiAmaliyyah <rizqia439@gmail.com>
2024-01-15 20:34:55 +0700

    membuat tabel product

--- create-tables.sql
+++ create-tables.sql
@@ -1,7 +1,9 @@
 -- Masuk ke database toko_online_db;
 use toko_online_db;
 
---- Table customers
+--- Membuat tabel-tabel:
+
+-- Tabel Customer
 CREATE TABLE customer (
   id_customer INT PRIMARY KEY AUTO_INCREMENT,
   customer_name VARCHAR(50) NOT NULL,
@@ -9,3 +11,21 @@ CREATE TABLE customer (
   city VARCHAR(100) NOT NULL,
   gender ENUM('Man', 'Woman') NOT NULL
 );
+
+-- Tabel Product
+CREATE TABLE product (
+  id_product INT PRIMARY KEY AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  store_name VARCHAR(50) NOT NULL,
+  product_price DECIMAL(10, 2) NOT NULL,
+  total_sold INT NOT NULL,
+  rating INT,
+  shipping_fee BOOLEAN,
+  stock INT,
+  FOREIGN KEY (store_name) REFERENCES store(store_name),
+  FOREIGN KEY (id_category) REFERENCES category(id_category)
+);
+
+
+
+
