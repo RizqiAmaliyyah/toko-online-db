ebce3bafca599646ef76243867cfecfbe783c95d
RizqiAmaliyyah <rizqia439@gmail.com>
2024-01-15 20:32:41 +0700

    membuat tabel customer

--- create-tables.sql
+++ create-tables.sql
@@ -1,3 +1,11 @@
 -- Masuk ke database toko_online_db;
 use toko_online_db;
 
+--- Table customers
+CREATE TABLE customer (
+  id_customer INT PRIMARY KEY AUTO_INCREMENT,
+  customer_name VARCHAR(50) NOT NULL,
+  age INT,
+  city VARCHAR(100) NOT NULL,
+  gender ENUM('Man', 'Woman') NOT NULL
+);
